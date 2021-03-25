d9baa93681a60e9eee7c1426770874055ca6ccd1
kalumekitsao <michael.kalume@savannahinformatics.com>
2021-03-25 12:31:13 +0300

    Go TDD Code Refactor
    - refactor code so that we introduce reusable components

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
 
-	if got !=want {
-		t.Errorf("got %q while we wanted %q", got, want)
-	}
+    assertCorrectMessage := func(t testing.TB, got, want string) {
+        // just a helper to help in reporting failure stack trace
+        t.Helper()
+        if got != want {
+            t.Errorf("got %q while we wanted %q", got, want)
+        }
+    }
+
+    t.Run("say hello to people", func(t *testing.T) {
+        got := Hello("Chris")
+        want := "Hello, Chris"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+        got := Hello("")
+        want := "Hello, World"
+        assertCorrectMessage(t, got, want)
+    })
 }
