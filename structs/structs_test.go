538f33c7b75811c60092c070a03d19f4ff3450f9
kalumekitsao <michael.kalume@savannahinformatics.com>
2021-03-26 18:08:11 +0300

    TDD introducing interfaces
    - Introduce interfaces and how they can be used

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -17,23 +17,22 @@ func TestStructs(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-    t.Run("Test find area of a rectangle", func(t *testing.T){
-        rectangle := Rectangle{10.00, 20.00}
-        got := rectangle.Area()
-        want := 200.00
-
-        if got != want {
-            t.Errorf("Got %.2f but expected %.2f", got, want)
-        }
-    })
-
-    t.Run("Test find area of a circle", func(t *testing.T){
-        circle := Circle{10.00}
-        got := circle.Area()
-        want := 314.1592653589793
+    checkArea := func(t testing.TB, shape Shape, want float64) {
+        t.Helper()
+        got := shape.Area()
 
         if got != want {
             t.Errorf("Got %g but expected %g", got, want)
         }
+    }
+
+    t.Run("Test find area of a rectangle", func(t *testing.T){
+        rectangle := Rectangle{10.00, 20.00}
+        checkArea(t, rectangle, 200.00)
+    })
+
+    t.Run("Test find area of a circle", func(t *testing.T){
+        circle := Circle{10.00}
+        checkArea(t, circle, 314.1592653589793)
     })
 }
